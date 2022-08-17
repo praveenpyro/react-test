0788ae182d1111e03aef56083a41ffbdfa74b014
praveenpyro <juluri.prvn@gmail.com>
2022-08-17 16:07:57 -0400

    1 file modified

--- tests/Child.test.js
+++ tests/Child.test.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import Child from '../../Child';
 it("renders with out crash", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Child></Child>, div)
+  ReactDOM.render(<Child></Child>, div);
 });
